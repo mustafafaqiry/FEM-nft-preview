b50b5c6c4df25787d73f14035a26107ab2b9b435
mustafafaqiry <faqirym9@gmail.com>
2022-03-06 19:48:57 +0500

    update

--- src/App.js
+++ src/App.js
@@ -8,39 +8,41 @@ import "./App.css";
 function App() {
   return (
     <>
-      <main>
-        <article className="main-image">
-          <img src={image} alt="" />
-          <div>
-            <img src={view} alt="" />
-          </div>
-        </article>
-        <article>
-          <h2>Equilibrium #3429</h2>
-          <p>Our Equilibrium collection promotes balance and calm.</p>
-          <ul>
-            <li>
-              <img src={ethereum} alt="" /> 0.041 ETH
-            </li>
-            <li>
-              <img src={clock} alt="" /> 3 days left
-            </li>
-          </ul>
-        </article>
-        <article className="avatar">
-          <img src={avatar} alt="" />
-          <p>
-            Creation of <span>Jules Wyvern</span>
-          </p>
-        </article>
-      </main>
-      <div className="attribution">
-        Challenge by{" "}
-        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
-          Frontend Mentor
-        </a>
-        . Coded by <a href="https://github.com/mustafafaqiry">Mustafa Faqiry</a>
-        .
+      <div className="card">
+        <main>
+          <article className="main-image">
+            <img src={image} alt="" />
+            <div>
+              <img src={view} alt="" />
+            </div>
+          </article>
+          <article>
+            <h2>Equilibrium #3429</h2>
+            <p>Our Equilibrium collection promotes balance and calm.</p>
+            <ul>
+              <li>
+                <img src={ethereum} alt="" /> 0.041 ETH
+              </li>
+              <li>
+                <img src={clock} alt="" /> 3 days left
+              </li>
+            </ul>
+          </article>
+          <article className="avatar">
+            <img src={avatar} alt="" />
+            <p>
+              Creation of <span>Jules Wyvern</span>
+            </p>
+          </article>
+        </main>
+        <div className="attribution">
+          Challenge by{" "}
+          <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+            Frontend Mentor
+          </a>
+          . Coded by{" "}
+          <a href="https://github.com/mustafafaqiry">Mustafa Faqiry</a>.
+        </div>
       </div>
     </>
   );
